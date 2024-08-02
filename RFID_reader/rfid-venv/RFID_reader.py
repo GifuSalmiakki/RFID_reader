e2a0efbbae4c51404ac3736358ffb0e76f0e78ff
Sisu Suominen <sisu.suominen@tuni.fi>
2024-08-02 16:54:56 +0300

    Added class functions

--- RFID_reader/rfid-venv/RFID_reader.py
+++ RFID_reader/rfid-venv/RFID_reader.py
@@ -18,7 +18,28 @@ class RFIDReader():
         self.spi.open(bus, device)
         self.spi.max_speed_hz = spd
 
+    def reinit(self):
+        self.reader.READER.spi = spidev.SpiDev()
+        self.reader.READER.spi.open(self.bus, self.device)
+        self.reader.READER.spi.max_speed_hz = self.spd
+        self.reader.READER.MFRC522_Init()
 
+    def close(self):
+        self.reader.READER.spi.close()
+
+    def addBoard(self, readerID, pin):
+        self.boards[readerID] = pin
+        GPIO.setup(pin, GPIO.OUT)
+        print(pin)
+
+    def selectBoard(self, readerID):
+        if not readerID in self.boards:
+            print("Reader ID" + readerID + " not found")
+            return False
+
+        for loop_id in self.boards:
+            GPIO.output(self.boards[loop_id], loop_id == rid)
+        return True
 
 def main():
     # refer to pins by "GPIO"-numbers on the board
@@ -26,6 +47,8 @@ def main():
     rfidReader = RFIDReader()
     readers = [("reader1", 5),
                ("reader2", 6)]
+    for r in readers:
+        RFIDReader.addBoard(r[0], r[1])
 
     cardsInPlace = False
     # reading each reader one at a time
@@ -44,4 +67,4 @@ def main():
             print("All cards in place :)")
             cardsInPlace = True
 
-GPIO.cleanup()
+    GPIO.cleanup()
