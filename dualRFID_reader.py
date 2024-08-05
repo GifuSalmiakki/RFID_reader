d511797be392aea303a0b324f187dffd3168f88e
Sisu Suominen <sisu.suominen@tuni.fi>
2024-08-06 00:35:42 +0300

    Starting work on implementing multiple readers

--- dualRFID_reader.py
+++ dualRFID_reader.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import spidev
-import signal
+from typing import Self
+from itertools import groupby
 
 # storing if cards have been read,
 # since that's the only thing we're concerned with
@@ -14,8 +15,7 @@ for card in range(CARD_AMOUNT):
     cardsRead[card] = False
 
 class RFIDReader():
-
-    def __init__(self, bus=0, device=0, spd=1000000):
+    def __init__(self, bus=0, device=0, spd=1000000) -> Self:
         self.reader = SimpleMFRC522()
         self.close()
         self.bus
@@ -24,22 +24,25 @@ class RFIDReader():
         self.bus = bus
         self.device = device
         self.spd = spd
-
-    def reinit(self):
+    def reinit(self) -> Self:
         self.reader.READER.spi = spidev.SpiDev()
         self.reader.READER.spi.open(self.bus, self.device)
         self.reader.READER.spi.max_speed_hz = self.spd
         self.reader.READER.MFRC522_Init()
 
-    def close(self):
+    def close(self) -> Self:
         self.reader.READER.spi.close()
 
-    def addBoard(self, readerID, pin):
+    def readCard(self, readerID) -> Self:
+        cardID, data = self.reader.read()
+        return cardID
+
+    def addBoard(self, readerID, pin) -> Self:
         self.boards[readerID] = pin
         GPIO.setup(pin, GPIO.OUT)
         print(pin)
 
-    def selectBoard(self, readerID):
+    def selectBoard(self, readerID) -> Self:
         if not readerID in self.boards:
             print("Reader ID" + readerID + " not found")
             return False
@@ -48,37 +51,26 @@ class RFIDReader():
             GPIO.output(self.boards[id], id == readerID)
         return True
 
-    def read(self, readerID):
-        if not self.selectBoard(readerID):
-            return None
-
-        self.reinit()
-        cardID, cardData = self.reader.read_no_block()
-        self.close()
-        return cardID
-
 def main():
     # refer to pins by "GPIO"-numbers on the board
     GPIO.setmode(GPIO.BCM)
     rfidReader = RFIDReader()
-
-    for r in readers:
-        RFIDReader.addBoard(RFIDReader, r[0], r[1])
-
     cardsInPlace = False
+
     # reading each reader one at a time
     while not cardsInPlace:
         for r in readers:
             try:
-                cardID = rfidReader.read(r[0])
+                cardID = rfidReader.readCard(r[0])
+                print("Card "+str(cardID)+" read")
                 # card was read, set corresponding value to True
-                if cardID != None:
-                    cardsRead[r] = True
+                cardsRead[r] = True
             except Exception as exception:
-                print("Execption: "+ exception)
+                print("Execption: "+ str(exception))
 
-        # all cards read on one pass, all cards in place
-        if cardsRead[0] == True:
+            # all cards read on one pass, all cards in place
+        values = groupby(cardsRead.values())
+        if all(value == True for value in values):
             print("All cards in place :)")
             cardsInPlace = True
 
@@ -86,3 +78,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
