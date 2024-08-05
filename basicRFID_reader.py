c4a0d9dc264233305eb6d02eef93ebe102768dac
Sisu Suominen <sisu.suominen@tuni.fi>
2024-08-05 21:12:41 +0300

    Checking if global variable works since attribute wont

--- basicRFID_reader.py
+++ basicRFID_reader.py
@@ -9,6 +9,7 @@ cardsRead = {}
 CARD_AMOUNT = 1
 GPIO_READER1 = 25
 readers = [("reader1", GPIO_READER1)]
+boards = {}
 
 for card in range(CARD_AMOUNT):
     cardsRead[card] = False
@@ -18,35 +19,33 @@ class RFIDReader():
     def __init__(self):
         self.reader = SimpleMFRC522()
         self.boards = {}
-        self.close()
 
     def read(self, readerID):
-        #if not self.selectBoard(readerID):
+        if not self.selectBoard(readerID):
             #return None
         cardID = self.reader.read()
-        self.close()
         return cardID
-'''''
+
     def addBoard(self, readerID, pin):
 
-        self.boards[readerID] = pin
+        boards[readerID] = pin
         GPIO.setup(pin, GPIO.OUT)
         print("Reader connected to pin: " +pin)
 
     def selectBoard(self, readerID):
-        if not readerID in self.boards:
+        if not readerID in boards:
             print("Reader ID" + readerID + " not found")
             return False
-        for id in self.boards:
-            GPIO.output(self.boards[id], id == readerID)
+        for id in boards:
+            GPIO.output(boards[id], id == readerID)
         return True
-'''
+
 def main():
     # refer to pins by "GPIO"-numbers on the board
     GPIO.setmode(GPIO.BCM)
     rfidReader = RFIDReader()
-    GPIO.setup(GPIO_READER1, GPIO.OUT)
     cardsInPlace = False
+
     # reading each reader one at a time
     while not cardsInPlace:
         for r in readers:
