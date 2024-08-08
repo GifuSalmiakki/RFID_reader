232e95fd452190d832a788612e1d55a423b1382a
Sisu Suominen <sisu.suominen@tuni.fi>
2024-08-08 14:29:03 +0300

    GPIO does not need to be specified

--- basicRFID_reader.py
+++ basicRFID_reader.py
@@ -1,42 +1,32 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import spidev
-import signal
 from typing import Self
-from itertools import groupby
-
-CARD_AMOUNT = 1
-GPIO_READER1 = 25
-readers = [("reader1", GPIO_READER1)]
 
 class RFIDReader():
 
     def __init__(self) -> Self:
         self.reader = SimpleMFRC522()
 
-    def readCard(self, readerID) -> Self:
+    def readCard(self) -> Self:
         cardID, data = self.reader.read()
         return cardID
 
 def main():
-    # refer to pins by "GPIO"-numbers on the board
-    GPIO.setmode(GPIO.BCM)
+
     rfidReader = RFIDReader()
     cardInPlace = False
-
-    # reading each reader one at a time
+    
     while not cardInPlace:
-        for r in readers:
-            try:
-                cardID = rfidReader.readCard(r[0])
-                print("Card "+str(cardID)+" read")
-                # card was read, set corresponding value to True
-                cardInPlace = True
-            except KeyboardInterrupt:
-                GPIO.cleanup()
+        try:
+            cardID = rfidReader.readCard()
+            print("Card "+str(cardID)+" read")
+            # card was read, set corresponding value to True
+            cardInPlace = True
+        except KeyboardInterrupt:
+            GPIO.cleanup()
 
-        if cardInPlace:
-            print("Card in place :)")
+    if cardInPlace:
+        print("Card in place :)")
 
     GPIO.cleanup()
 
