93d0728eba6db229859a571853bcf24d5ca91d49
Sisu Suominen <sisu.suominen@tuni.fi>
2024-08-08 14:08:28 +0300

    Reading one RFID card works

--- basicRFID_reader.py
+++ basicRFID_reader.py
@@ -1,13 +1,8 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import spidev
-import signal
 from typing import Self
-from itertools import groupby
+import time
 
-# storing if cards have been read,
-# since that's the only thing we're concerned with
-cardsRead = {}
 CARD_AMOUNT = 1
 GPIO_READER1 = 25
 readers = [("reader1", GPIO_READER1)]
@@ -17,7 +12,7 @@ class RFIDReader():
     def __init__(self) -> Self:
         self.reader = SimpleMFRC522()
 
-    def readCard(self) -> Self:
+    def readCard(self, readerID) -> Self:
         cardID, data = self.reader.read()
         return cardID
 
@@ -29,18 +24,21 @@ def main():
 
     # reading each reader one at a time
     while not cardInPlace:
-
-        try:
-            cardID = rfidReader.readCard()
-            print("Card "+str(cardID)+" read")
-            # card was read, set corresponding value to True
-            cardInPlace = True
-        except Exception as exception:
-            print("Execption: " + str(exception))
+        for r in readers:
+            try:
+                cardID = rfidReader.readCard(r[0])
+                print("Card "+str(cardID)+" read")
+                # card was read, set corresponding value to True
+                cardInPlace = True
+            except KeyboardInterrupt:
+                GPIO.cleanup()
 
         if cardInPlace:
             print("Card in place :)")
+            cardsInPlace = True
 
+        time.sleep(1)
+        
     GPIO.cleanup()
 
 if __name__ == "__main__":
