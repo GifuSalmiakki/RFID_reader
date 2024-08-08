00a25c8b4afdec43ca96c59618d24401e243d749
Sisu Suominen <sisu.suominen@tuni.fi>
2024-08-08 23:53:15 +0300

    Working multireader code :)

--- multiRFID_reader.py
+++ multiRFID_reader.py
@@ -4,17 +4,12 @@ import spidev
 from typing import Self
 from itertools import groupby
 import time
+from PIL import Image
 
-# storing if cards have been read,
-# since that's the only thing we're concerned with
-cardsRead = {}
-CARD_AMOUNT = 1
+CARD_AMOUNT = 2
 GPIO_READER1 = 5
 GPIO_READER2 = 6
 readers = [("reader1", GPIO_READER1), ("reader2", GPIO_READER2)]
-
-for card in range(CARD_AMOUNT):
-    cardsRead[card] = False
 
 class RFIDReader():
     def __init__(self, bus=0, device=0, speed=1000000) -> Self:
@@ -64,30 +59,32 @@ def main():
 
     cardsInPlace = False
     rfidReader = RFIDReader()
+    # Chip select pin
     GPIO.setup(8, GPIO.OUT)
+
     for r in readers:
         rfidReader.addBoard(r[0], r[1])
 
     # reading each reader one at a time
     while not cardsInPlace:
+
+        bothCardsRead = 0
         for r in readers:
             try:
                 cardID = rfidReader.read(r[0])
                 if cardID != None:
                     print("Card "+str(cardID)+" read")
                     # card was read, set corresponding value to True
-                    cardsRead[r] = True
+                    bothCardsRead += 1
             except Exception as exception:
                 print("Execption: "+ str(exception))
 
-            # all cards read on one pass, all cards in place
-        values = groupby(cardsRead.values())
-        if all(value == True for value in values):
-            print("All cards in place :)")
+        # all cards read on one pass, all cards in place
+        if bothCardsRead == 2:
             cardsInPlace = True
+            print("All cards in place! :)")
 
         time.sleep(1)
-
     GPIO.cleanup()
 
 if __name__ == "__main__":
